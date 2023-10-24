60039d4524b13b0c7e856c49de76bb4defe2055c
Andrew Guest <andrew@aguest.me>
2023-10-24 12:46:10 -0500

    Reorganizing and adding Swift

---
+++ swift/01 - variables and constants.swift
@@ -0,0 +1,32 @@
+// let -> Defines a constant value that cannot be changed.
+// var -> Defines a variable value that can be changed.
+
+// Declaring a CONSTANT using the `let` keyword
+let doubleVar: Double = 2.0
+
+// Declaring a VARIABLE using the `var` keyword
+var intVar: Int = 5
+
+
+/*
+
+Data types:
+
+Character: 16-bit unicode character ("A", "z")
+
+String: Textual data ("hello world")
+
+Int: Integer number (3, -24)
+
+Float: 32-bit floating-point number (2.4, 3.14, -23.21)
+
+Double: 64-bit floating-point number (2.422342412414)
+
+Bool: Any of two values: `true` or `false` (true, false)
+
+*/
+
+
+// Print variable's value
+print(doubleVar)  // 2.0
+print(intVar)  // 5
