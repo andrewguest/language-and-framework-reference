5530504cdc41f9fd51f55a64a92579e40f4d3665
Andrew Guest <andrew@aguest.me>
2023-10-28 22:55:03 -0500

    Updated Xcode project

--- swift/apps/Code History/Code History/ContentView.swift
+++ swift/apps/Code History/Code History/ContentView.swift
@@ -7,14 +7,62 @@
 
 import SwiftUI
 
+let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .foregroundColor(Color.green)
-            .multilineTextAlignment(.leading)
-            .padding()
+        ZStack {
+            // The safe areas are the very top and very bottom of the screen.
+            // This allows the entire screen to be this color
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: { print("Tapped on Choice 1") }, label: {
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(.cyan, width: 4)
+                    })
+                    Button(action: { print("Tapped on Choice 2") }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(.cyan, width: 4)
+                    })
+                    Button(action: { print("Tapped on Choice 3") }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(.cyan, width: 4)
+                    })
+                    Button(action: { print("Tapped on Choice 4") }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(.cyan, width: 4)
+                    })
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
 }
 
