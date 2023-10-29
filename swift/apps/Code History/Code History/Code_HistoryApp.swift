30aba526dcdb9ff460812fdc75bc8c8742ae9fb2
Andrew Guest <andrew@aguest.me>
2023-10-28 19:54:07 -0500

    Updated .gitignore for Xcode

---
+++ swift/apps/Code History/Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Andrew on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
